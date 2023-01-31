3e2fb935473b781e385558bb18a0d9bd72e33bf8
Bradlee King <Kingofbrad@gmail.com>
2023-01-31 22:09:33 +0000

    Initial Commit

---
+++ AllNote/AllNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllNoteApp.swift
+//  AllNote
+//
+//  Created by Bradlee King on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AllNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
