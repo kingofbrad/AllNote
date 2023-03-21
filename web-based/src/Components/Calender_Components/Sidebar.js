1809ee62191027c813f5d9f872c9b66a0bbd1ac6
Bradlee King <Kingofbrad@gmail.com>
2023-03-21 15:39:45 +0000

    Added The web-based version of the allnote

---
+++ web-based/src/Components/Calender_Components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateEventButton from "./CreateEventButton";
+import SmallCalender from "./SmallCalender";
+import Labels from "./Labels";
+const Sidebar = () => {
+  return (
+    <aside className="border p-5 w-64">
+      <CreateEventButton />
+      <SmallCalender />
+      <Labels />
+    </aside>
+  );
+};
+
+export default Sidebar;
---
+++ web-based/src/controllers/firebase.js
@@ -0,0 +1,21 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "firebase/app";
+ import { getFirestore } from "firebase/firestore";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+   authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+   projectId: "XXXXXXXXXXXXXXXXX",
+   storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXX",
+ 
+   messagingSenderId: "XXXXXXXXXXXXXXX",
+   appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+ };
+ // Initialize Firebase
+ 
+ const app = initializeApp(firebaseConfig);
+ // Export firestore database
+ // It will be imported into your react app whenever it is needed
+ export const db = getFirestore(app);
